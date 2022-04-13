e65c934640c64a3179b489592d00486fefb0b7f8
DownieTown <lukas.downes@student.odisee.be>
2022-04-13 17:52:39 +0200

    making of

---
+++ angryAndroids.js
@@ -0,0 +1,22 @@
+"use strict";
+(function() {
+    const androidBody = document.querySelectorAll(".android div");
+    const androidEyes = document.querySelectorAll(".eyes");
+    const frame = document.querySelector(".frame");
+    const glare = () => {
+        androidEyes[0].style.backgroundColor = "red";
+        androidEyes[1].style.backgroundColor = "red";
+        frame.style.borderColor = "red";
+    }
+    const unglare = () => {
+        androidEyes[0].style.backgroundColor = "var(--background)";
+        androidEyes[1].style.backgroundColor = "var(--background)";
+        frame.style.borderColor = "lime";
+    }
+
+
+    for (let i = 0; i < androidBody.length; i++) {
+        androidBody[i].addEventListener('mouseout', unglare)
+        androidBody[i].addEventListener('mouseover',glare)
+    }
+})();
