2961fcd1eaf88b0db2507e7eaafc2c57ac8e636d
DownieTown <95034909+DownieTown@users.noreply.github.com>
2022-04-14 11:56:44 +0200

    Update angryAndroids.js

--- angryAndroids.js
+++ angryAndroids.js
@@ -3,6 +3,8 @@
     const androidBody = document.querySelectorAll(".android div");
     const androidEyes = document.querySelectorAll(".eyes");
     const frame = document.querySelector(".frame");
+    const tToggle = document.querySelector(".dtToggle");
+    const root = document.querySelector(":root");
     const glare = () => {
         androidEyes[0].style.backgroundColor = "red";
         androidEyes[1].style.backgroundColor = "red";
@@ -13,8 +15,11 @@
         androidEyes[1].style.backgroundColor = "var(--background)";
         frame.style.borderColor = "var(--green)";
     }
+    const tSwitch = () => {
+        root.classlist.toggle('inverse');
+    }
 
-
+    tToggle.addEventListener('click',tSwitch);
     for (let i = 0; i < androidBody.length; i++) {
         androidBody[i].addEventListener('mouseout', unglare)
         androidBody[i].addEventListener('mouseover',glare)
